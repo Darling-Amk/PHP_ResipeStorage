b5005eb7324c1851f8cf52fb39ef53e47a01b357
Darling-Amk <kruchinin.nikita.09@gmail.com>
2022-02-14 16:59:09 +0300

    blabla

--- index.php
+++ index.php
@@ -1,76 +1,107 @@
-
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    
-    <link rel="apple-touch-icon" sizes="76x76" href="/list/ico/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/list/ico/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/list/ico/favicon-16x16.png">
-    <link rel="manifest" href="/list/ico/site.webmanifest">
-    <link rel="mask-icon" href="/list/ico/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href = style.css>
-
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/info.css">
+    <link rel="stylesheet" href="css/resipe.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">    
-
-
-    <title>Список</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+    <title>Document</title>
 </head>
 <body>
-    <header>
-        <img src="list/img/1.gif" alt="Логотип с popcat" class="logo">
-        <h2>Список выполненных лабораторных работ</h2>
-        <img src="list/img/1.gif" alt="Логотип с popcat" class="logo mir">
+    <!-- header -->
+    <header class = "header__container">
+        <div class="header__label__conteiner">
+            <h2 class="header__label">Genshin Impact</h2>
+            <h1 class="header__title">Recipe Storage</h1>
+        </div>
+        <form  class = "header__form" action="">
+            <input class = "header__form__username" type="text">
+            <input class = "header__form__password" type="password">
+            <input class = "header__form__button" type="button" value="help">
+        </form>
     </header>
-    <hr>
-    
-    <div class="content">
-        <ol>
-            
-            <li>
-                <h3>Лабораторная работа:</h3>
-                <ul>
-                    <li><a href="list/1/simple_start.php">Таблица</a></li>
+    <!-- /header -->
 
-                </ul>  
-            </li>
-            <li>
-                <h3>Лабораторная работа:</h3>
-                <ul>
-                    <li><a href="list/2/piramida.php">Прирамида</a></li>
-                    <li><a href="list/2/demotivator.php">Демотиватор</a></li>
-                    <li><a href="list/2/graycat.php">Серый кот</a></li>
-                </ul>
-            </li>
-            
-            <li>
-                <h3>Лабораторная работа:</h3>
-                <ul>
-                    <li><a href="list/3/info.php">info(напрямую без сессии не пускает)</a></li>
-                    <li><a href="list/3/login.php">login</a></li>
-                    <li><a href="list/3/upload.html">upload</a></li>
-                </ul>
-            </li>
-            <li><a href="list/4/index.php">Лаба</a></li>
-            <li><a href="list/5/index.php">Лаба</a></li>
-        </ol>   
-        <?php
-        $s =mt_rand(1,2);
-            echo "<img class='child' src='list/img/{$s}.png'>";
-        ?>
+
+
+
+    <!-- sidebar -->
+    <div class="sidebar__container">
+            <ul class="sidebar__menu">
+                <li class="sidebar__label">
+                    <div class="cnt">
+                    <img src="img/globe.svg" alt="Logo">  RS
+                    </div>
+                </li>
+                <li class="sidebar__item">Яблоки</li>
+                <li class="sidebar__item">Груши</li>
+                <li class="sidebar__item">Чай</li>
+                <li class="sidebar__item">Кофе</li>
+                <li class="sidebar__item">Монштат</li>
+                <li class="sidebar__item">Яблоки</li>
+                <li class="sidebar__item">Груши</li>
+                <li class="sidebar__item">Чай</li>
+                <li class="sidebar__item">Кофе</li>
+                <li class="sidebar__item">Монштат</li>
+                <li class="sidebar__item">Яблоки</li>
+                <li class="sidebar__item">Груши</li>
+                <li class="sidebar__item">Чай</li>
+                <li class="sidebar__item">Кофе</li>
+                <li class="sidebar__item">Монштат</li>
+                <li class="sidebar__item">Яблоки</li>
+                <li class="sidebar__item">Груши</li>
+                <li class="sidebar__item">Чай</li>
+                <li class="sidebar__item">Кофе</li>
+                <li class="sidebar__item">Монштат</li>
+                <li class="sidebar__item">Яблоки</li>
+                <li class="sidebar__item">Груши</li>
+                <li class="sidebar__item">Чай</li>
+                <li class="sidebar__item">Кофе</li>
+                <li class="sidebar__item">Монштат</li>
+
+                
+            </ul>        
     </div>
-    <hr>
-    <em>
-        <p align = "right">Зима 2022</p>
-        <!-- <p align = "right">Кручинин Никита 22203</p> -->
-    </em>
-   
+    <!-- /sidebar -->
+    
+
+
+    
+    
+    
+    <div class="info__container">
+        <h1 class="info__name">Миндальный тофу</h1>
+        <div class="info__container__text">
+            <p>Миндальный тофу — это еда, которую может приготовить игрок. Рецепт Миндального тофу можно получить, исследуя кухонный стол на кухне Постоялого двора «Ваншу». Блюдо Миндальный тофу также можно купить у Верр Голдет на Постоялом дворе «Ваншу» за 1,550 Моры (максимум 2 в день).
+                В зависимости от качества, Миндальный тофу увеличивает атаку всех членов отряда на 66 / 81 / 95 в течение 300 секунд. Как и у большей части еды, эффект этого блюда не действует на персонажей других игроков в Режиме совместной игры.
+                
+                Когда Сяо готовит Миндальный тофу, у него есть шанс приготовить «Сладкий сон» вместо этого.</p>
+        </div>
+    </div>
+    
+
+
+
+    <div class="recipe__container">
+        <ul class="recipe__list">
+            <li class="recipe__img">
+                <img src="img/eat.png" alt="">
+            </li>
+            <li class="recipe__item">Молоко х3 <a href="#">Vieu</a></li>
+            <li class="recipe__item">Сахар х1 <a href="#">Vieu</a></li>
+            <li class="recipe__item">Мендаль х3 <a href="#">Vieu</a></li>
+            <li class="recipe__item">Молоко х1 <a href="#">Vieu</a></li>
+
+        </ul>
+    </div>
+    
 </body>
 </html>
